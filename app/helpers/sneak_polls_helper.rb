861c3d57c18ab49c5199bd5c71ce6440268d5bab
sodercober <sodercober@gmail.com>
2011-06-16 21:46:24 +0000

    Radio select

--- app/helpers/sneak_polls_helper.rb
+++ app/helpers/sneak_polls_helper.rb
@@ -20,4 +20,31 @@ module SneakPollsHelper
     grade ? number_with_precision(grade, :precision => 2) : t(:label_sneak_poll_not_applicable_abbr)
   end
 
+  def radio_select(form, field, collection, options = {})
+    ''.tap do |html|
+      html << form.label(field, "#{l("field_#{field}")}:", options.merge(:class => form.object.errors.on("#{field}_notes") ? 'invalid' : nil))
+      if blank = options.delete(:include_blank)
+        html << content_tag(:span, :class => 'radio') do
+          form.radio_button(field, '', options) +
+              form.label(field, blank, options.merge(:value => '', :class => 'inline')) unless blank.is_a?(TrueClass)
+        end
+      end
+      collection.each do |element|
+        html << content_tag(:span, :class => 'radio') do
+          form.radio_button(field, element, options.merge(:id => "#{form.object_name}_#{field}_#{element}")) +#TODO: Remove :id after migration to Rails 3
+              form.label(field, element, options.merge(:for => "#{form.object_name}_#{field}_#{element}", :class => 'inline')) #TODO: Remove :id after migration to Rails 3
+        end
+      end
+    end
+  end
+
+  def grade_field(form, field, options = {})
+    content_tag(:p, :class => form.object.errors.on("#{field}_notes") ? 'invalid' : nil) do
+      radio_select(form, field, SneakPollVote::GRADES_RANGE.to_a, :include_blank => t(:label_sneak_poll_not_applicable_abbr), :disabled => !can_vote?, :readonly => !can_vote?) +
+          tag(:br) +
+          form.text_area("#{field}_notes", :rows => 2, :disabled => !can_vote?, :readonly => !can_vote?) +
+          (form.object.errors.on("#{field}_notes") ? tag(:br) + form.object.errors.on("#{field}_notes") : '')
+    end
+  end
+
 end
